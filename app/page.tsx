3f9ce82c04be162e21b423c7d1afc52306cc3add
Bridger Tower <bridgertower@gmail.com>
2023-12-11 12:18:32 -0700

    feat: images from notion db

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { notion } from "@/lib/notion";
 import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   const data: QueryDatabaseResponse = await notion.databases.query({
@@ -13,13 +14,19 @@ export default async function Home() {
         {/* TO-DO: Update this to the correct type to avoid using 'any' */}
         {data.results.map((result: any) => (
           <div key={result?.properties?.id?.unique_id?.number}>
+            <Image
+              src={result.properties?.image?.files[0]?.file?.url}
+              alt={`Image for ${result.properties?.name?.title[0]?.text?.content}`}
+              width={700}
+              height={500}
+            />{" "}
             <p>ID: {result.properties?.id?.unique_id?.number}</p>
             <p>updated: {result.properties?.updated_at?.last_edited_time}</p>
             <p>created: {result.properties?.created_at?.created_time}</p>
             <p>status: {result.properties?.status?.select?.name}</p>
             <p>
               description:{" "}
-              {result.properties?.description?.rich_text[0]?.text?.content}
+              {result.properties?.description?.rich_text[0]?.text?.content}å
             </p>
             <p>url: {result.properties?.url?.url}</p>
             <p>name: {result.properties?.name?.title[0]?.text?.content}</p>
